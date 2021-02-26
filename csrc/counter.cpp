457b1715cfc8920ff1097ce421b34be8f18c0c93
Ciaran Lappin <Ciaran.Lappin@microchip.com>
2021-02-26 10:49:17 +0000

    Added tick function to csrc/counter.cpp.

--- csrc/counter.cpp
+++ csrc/counter.cpp
@@ -4,32 +4,40 @@
 #include "verilated.h"
 
 
+// adding a tick function for the clock without vcd code.
+
+void tick(Vcounter *tb)
+{
+    tb->eval();
+    tb->clock = 1;
+    tb->eval();
+    tb->clock = 0;
+    tb->eval();
+}
+
+
 int main(int argc, char **argv) {
-	int count_out;
+    int count_out;
     // Call commandArgs first!
-	Verilated::commandArgs(argc, argv);    
+    Verilated::commandArgs(argc, argv);
 
-	// Instantiate our design
-	Vcounter *tb = new Vcounter;
+    // Instantiate our design
+    Vcounter *tb = new Vcounter;
 
-	// Need a clock pulse
-    
+    // Need a clock pulse
     count_out = tb->out1 ;
     // Reset the counter
-    tb->reset = 1; 
+    tb->reset = 1;
     tb->eval(); // Evaluate the model.
     tb->reset = 0;
     tb->eval();// Evaluate the model.
-    
-    // Simple loop which toggles the clock and prints the 
-    // value of out1 in the RTL. 
+
+    // Simple loop which toggles the clock and prints the
+    // value of out1 in the RTL.
     for (int x=0; x < 100; x++)
-    {        
-        tb->clock = 1;
-        tb->eval();// Evaluate the model.
-        tb->clock = 0;
-        tb->eval();// Evaluate the model.
-        printf("Output: %d, Reset: %d\n", tb->out1, tb->reset);        
+    {
+        tick(tb); // calling the clock tick function.
+        printf("Output: %d, Reset: %d\n", tb->out1, tb->reset);
     }
 
 }
